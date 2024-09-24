4a7e39ed226473027858304b020e3cbb533e60b1
daleale <daleale.se@gmail.com>
2024-09-24 20:53:36 -0300

    Coding form validation feedback from Mortgage Calculator

---
+++ Mortgage Repayment Calculator/js/validateForm.js
@@ -0,0 +1,78 @@
+const validForm = () => {
+    const inputElements = document.querySelectorAll("input[type='number']")
+    for (const input of inputElements) {
+        if (input.value === "") return false
+    }
+    return true && radioInputWasSelected()
+}
+
+const radioInputWasSelected = () => {
+    const radioInputs = document.querySelectorAll("input[name='mortgage-type']")
+    for (let input of radioInputs) {
+        if (input.checked) return true
+    }
+    return false
+}
+
+const addRequiredRadio = () => {
+    const typeContainer = document.querySelector(".type-field")
+    typeContainer.querySelector(".required").classList.remove("hidden")
+}
+
+const removeRequiredRadio = () => {
+    const typeContainer = document.querySelector(".type-field")
+    typeContainer.querySelector(".required").classList.add("hidden")
+}
+
+const requiredValues = () => {
+    if (!radioInputWasSelected()) {
+        addRequiredRadio()
+    }
+    const inputElements = document.querySelectorAll("input[type='number']")
+    inputElements.forEach(input => verifyValue(input))    
+}
+
+const handleSubmit = e => {
+    e.preventDefault()
+    if (validForm()) {
+        console.log("valid");
+
+        // do calculation
+
+    } else {
+        console.log("invalid");
+        requiredValues()
+    }
+}
+
+const addRequiredField = (name) => {
+    const fieldContainer = document.querySelector(`.${name}-field`);
+    fieldContainer.querySelector(".field").classList.add("required-field")
+    fieldContainer.querySelector(".label-block").classList.add("required-label-block")
+    fieldContainer.querySelector(".required").classList.remove("hidden")
+}
+
+const removeRequiredField = (name) => {
+    const fieldContainer = document.querySelector(`.${name}-field`);
+    fieldContainer.querySelector(".field").classList.remove("required-field")
+    fieldContainer.querySelector(".label-block").classList.remove("required-label-block")
+    fieldContainer.querySelector(".required").classList.add("hidden")
+}
+
+const verifyValue = (input) => {
+    const value = input.value
+    value === "" ? addRequiredField(input.name) : removeRequiredField(input.name)
+}
+
+const init = () => {
+    const form = document.querySelector("form")
+    form.addEventListener("submit", handleSubmit)
+
+    const radioInputs = document.querySelectorAll("input[name='mortgage-type']")
+    radioInputs.forEach(input => input.addEventListener("change", removeRequiredRadio))
+    
+    const inputElements = document.querySelectorAll("input[type='number']")
+    inputElements.forEach(input => input.addEventListener("blur", () => verifyValue(input)))
+}
+
+export default init
