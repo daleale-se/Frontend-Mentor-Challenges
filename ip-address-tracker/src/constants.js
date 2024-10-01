5d48232f12301f10759a6a2eb12af6dab4e1e060
daleale <daleale.se@gmail.com>
2024-10-01 16:46:41 -0300

    Setting enviroment variables and  uploading dist folder

--- ip-address-tracker/src/constants.js
+++ ip-address-tracker/src/constants.js
@@ -1,1 +1,1 @@
-export const GEO_IPIFY_APY = "https://geo.ipify.org/api/v2/country,city?apiKey=at_TcpoddlPwF83b60xBB1xeHFlKH0Kl"
+export const GEO_IPIFY_APY = `https://geo.ipify.org/api/v2/country,city?apiKey=${import.meta.env.VITE_IPIFY_APIKEY}`
